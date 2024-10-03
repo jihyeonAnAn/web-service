74cb21955c4e1e48e1970189804af6c98916de08
Jihyeon An <jh011007@naver.com>
2024-10-03 21:54:02 +0900

    JPA Auditing으로 생성시간/수정시간 자동화하기

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.domain.posts;
 
+import com.jojoldu.book.springboot.domain.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/test/java/com/jojoldu/book/springboot/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.jojoldu.book.springboot;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@EnableJpaAuditing
+@SpringBootTest
+class ApplicationTest {
+
+}
